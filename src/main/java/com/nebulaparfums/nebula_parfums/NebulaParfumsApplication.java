a84107b36cf2460359dd008ff26bbc9c91223d43
www.lui81@gmail.com <www.lui81@gmail.com>
2026-04-02 13:06:32 -0600

    Inicializacion del proyecto

---
+++ src/main/java/com/nebulaparfums/nebula_parfums/NebulaParfumsApplication.java
@@ -0,0 +1,13 @@
+package com.nebulaparfums.nebula_parfums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaParfumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulaParfumsApplication.class, args);
+	}
+
+}
