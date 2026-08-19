c65e18fcc491b0986b0a62b179e15da5bdfe3e06
thecakeisalie81 <www.lui81@gmail.com>
2026-08-19 13:44:05 -0600

    Modificacion de controlador de direcciones de envio

--- src/main/java/com/nebulaparfums/nebula_parfums/controller/DireccionEnvioController.java
+++ src/main/java/com/nebulaparfums/nebula_parfums/controller/DireccionEnvioController.java
@@ -1,51 +1,51 @@
-/*
 package com.nebulaparfums.nebula_parfums.controller;
 
-import com.nebulaparfums.nebula_parfums.model.DireccionEnvio;
-import com.nebulaparfums.nebula_parfums.model.Usuario;
+import com.nebulaparfums.nebula_parfums.dto.DireccionDTO;
+import com.nebulaparfums.nebula_parfums.dto.UsuarioDTO;
+import com.nebulaparfums.nebula_parfums.mapper.Mapper;
 import com.nebulaparfums.nebula_parfums.service.interfaces.IDireccionEnvioService;
-import com.nebulaparfums.nebula_parfums.service.interfaces.IUsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/direccion")
 public class DireccionEnvioController {
-    @Autowired
-    private IDireccionEnvioService iDireccionEnvioService;
 
-    @Autowired
-    private IUsuarioService iUsuarioService;
+    private final IDireccionEnvioService iDireccionEnvioService;
 
-    @GetMapping("/direccion/buscar")
-    public DireccionEnvio buscarDireccionEnvio(@RequestParam("id") Integer id){
-        return iDireccionEnvioService.getDireccionEnvioById(id);
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<DireccionDTO> buscarDireccionEnvio(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(iDireccionEnvioService.getDireccionEnvioById(id));
     }
 
-    @PostMapping("/direccion/crear")
-    public String crearDireccionEnvio(@RequestBody DireccionEnvio direccionEnvio){
-        iDireccionEnvioService.saveDireccionEnvio(direccionEnvio);
-        return "Direccion Envio creado con sucesso";
+    @GetMapping("/midireccion")
+    public ResponseEntity<DireccionDTO> obtenerMiDireccion(@AuthenticationPrincipal UsuarioDTO usuario) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Mapper.toDTO(usuario.getDireccionEnvio()));
     }
 
-    @GetMapping("/direccion/mia")
-    public DireccionEnvio obtenerMiDireccion(Authentication authentication) {
-        String email = authentication.getName();
-        Usuario user = iUsuarioService.getUsuarioByEmail(email);
-        return user.getDireccionEnvio();
+    @PutMapping("/editar")
+    public ResponseEntity<DireccionDTO> editarMiDireccion(
+                                    @RequestBody DireccionDTO direccionEnvio,
+                                    @AuthenticationPrincipal UsuarioDTO usuario) {
+
+        DireccionDTO direccion = iDireccionEnvioService.editarDireccionEnvio(
+                usuario.getDireccionEnvio().getId_direccion(), direccionEnvio
+        );
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(direccion);
     }
 
-    @PutMapping("/direccion/editar")
-    public String editarMiDireccion(@RequestBody DireccionEnvio direccionEnvio, Authentication authentication) {
-        String email = authentication.getName();
-        iDireccionEnvioService.editarDireccionPorEmail(email, direccionEnvio);
-        return "Direccion Envio editado con sucesso";
-    }
-
-    @DeleteMapping("/direccion/borrar")
-    public String borrarDireccionEnvio(@RequestParam("id") Integer id){
+    @DeleteMapping("/borrar/{id}")
+    public ResponseEntity<String> borrarDireccionEnvio(@PathVariable Integer id){
         iDireccionEnvioService.deleteDireccionEnvioById(id);
-        return "Direccion Envio borrado con sucesso";
+        return  ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body("Dirección de envío borrada correctamente");
     }
 }
-*/
